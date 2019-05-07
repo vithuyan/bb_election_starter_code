cc346b11c0a08e87be4e7d0c12f18a812ebb877c
Vithuyan Sivananthan <vithuyan.s@outlook.com>
2019-05-07 13:03:50 -0400

    done

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,54 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const candidateList = document.getElementById('list');
+ const refreshButton = document.querySelector('#refresh-button');
 
+ refreshButton.addEventListener('click', function() {
+   axios.get('https://bb-election-api.herokuapp.com/')
+   .then(function(response) {
+     candidateList.innerText = "";
+
+   response.data.candidates.forEach((candidate) => {
+     const individual = document.createElement('li');
+     individual.innerHTML = (`${candidate.name}: ${candidate.votes} votes`);
+     candidateList.appendChild(individual);
+
+     const form = document.createElement('form');
+     form.method = 'POST';
+     form.action = 'https://bb-election-api.herokuapp.com/vote';
+
+     const hidden = document.createElement('input');
+     const button = document.createElement('button');
+     button.innerText = `Vote for ${candidate.name}!`
+     hidden.type = 'hidden';
+     hidden.name = 'name';
+     hidden.value = candidate.name
+     form.appendChild(hidden);
+     form.appendChild(button);
+     individual.appendChild(form);
+
+   });
+ });
+
+});
+});
+
+});
+document.addEventListener('submit', function(event) {
+
+  event.preventDefault();
+
+  let name = event.target.querySelector('input[type=hidden]').value;
+  console.log(name);
+
+  axios({
+  method: 'post',
+  url: 'https://bb-election-api.herokuapp.com/vote',
+  data: { 'name': name }
+
+}).then(function(response) {
+  console.log(response);
+
+}).catch(function(response) {
+  alert('Request Failed');
+})
 });
